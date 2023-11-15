8e77d0256ef6616f441b667304357cf1e56fcfc4
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2023-11-15 00:37:29 -0500

    semantics

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 });
 
 // Data directory paths
-const DATA_DIR = path.join(__dirname, 'json');
+const DATA_DIR = path.join(':3000' ,__dirname, 'json');
 const SUPERHERO_INFO_FILE = path.join(DATA_DIR, 'superhero_info.json');
 const SUPERHERO_POWERS_FILE = path.join(DATA_DIR, 'superhero_powers.json');
 
